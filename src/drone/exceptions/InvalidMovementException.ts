96e1ded4386b6e22de42037d54502d46066c16d5
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-03 02:28:45 -0300

    refactor: Improve error message for InvalidMovementException

--- src/drone/exceptions/InvalidMovementException.ts
+++ src/drone/exceptions/InvalidMovementException.ts
@@ -1,5 +1,5 @@
 export class InvalidMovementException extends Error {
   constructor() {
-    super("Invalid Movement!");
+    super("The drone's movement must be one of these values: M, L or R");
   }
 }
